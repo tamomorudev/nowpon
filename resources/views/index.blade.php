1038b0e39a2dab34c078142e285d7d466c5a0e01
Hiroki Morita <tamomoru.dev@gmail.com>
2026-02-11 15:07:08 +0900

    トップページ 新着とマイエリアだけ残す（その他コメントアウトで一旦退避）

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -484,9 +484,9 @@
 
     <div class="filter-buttons">
         <button class="active" onclick="location.href='/site/couponlist'">新着</button>
-        <button onclick="location.href='/site/couponlist'">お気に入り</button>
+        <!-- <button onclick="location.href='/site/couponlist'">お気に入り</button> -->
         <button onclick="location.href='/site/couponlist?search=area'">マイエリア</button>
-        <button onclick="location.href='/site/couponlist'">お得なクーポン</button>
+        <!-- <button onclick="location.href='/site/couponlist'">お得なクーポン</button> -->
     </div>
 
     <!-- クーポンリスト -->
